21850f6197c536b5229e274b20c94b136ae2793e
JUAN-PC\Juan <juuan.olmedo@hotmail.com>
2018-04-07 13:55:39 -0300

    Organizacion en Carpetas

---
+++ JavaScript/cargarPlantel.js
@@ -0,0 +1,30 @@
+$(document).ready(getArchivo);
+
+function getArchivo(){
+  $.get("https://uns-iaw-2018-com16.github.io/TorneoFutbol/plantelEquipo1.json", function(data, status){
+      cargarPlantelEquipo(data);
+  });
+}
+
+function cargarPlantelEquipo(data){
+  var i;
+  var plantelEquipo = ""; 
+  for(i = 0; i < data.length; i++){
+    plantelEquipo+="<div class=\"media-left media-middle\">"+
+    			"<a>"+
+    		  		"<img class=\"media-object\" src=\""+data[i].foto+"\" alt=\"Imagen no disponible\">"+
+    		  "</a>"+
+    		 "</div>"+
+    		 "<div class=\"media-body\">"+
+    		  	 "<h4 class=\"media-heading\">"+data[i].apellido+", "+data[i].nombre+"</h4>"+
+    		  		"<p> Fecha de Nacimiento: "+data[i].fechaNacimiento+
+    		  			 " </br> Edad: "+data[i].edad+
+    		  			 " </br> Altura: "+data[i].altura+
+    		  			 " </br> Peso: "+data[i].peso+
+    		  			 " </br> Camiseta: "+data[i].numeroCamiseta+
+    		  			 " </br> Pie Habil: "+data[i].pieHabil+
+    		  			 " </br> Posicion: "+data[i].posicion+"</p>"+
+    		 "</div> </br>";		
+  }
+  document.getElementById("plantel").innerHTML = plantelEquipo;
+}
---
+++ JavaScript/cargarTablaAmarillas.js
@@ -0,0 +1,22 @@
+$(document).ready(getArchivo);
+
+function getArchivo(){
+  $.get("https://uns-iaw-2018-com16.github.io/TorneoFutbol/amarillas.json", function(data, status){
+      crearTablaAmarillas(data);
+  });
+}
+
+function crearTablaAmarillas(data){
+  var i;
+  var tablaAmarillas = "<tr>"+
+                          "<th>Jugador</th>"+
+                          "<th>Equipo</th>"+
+                          "<th>Amarillas</th>"+
+                       "</tr>";
+  for(i = 0; i < data.length; i++){
+    tablaAmarillas+= "<tr><td>" + data[i].jugador +
+                    "</td><td>" + data[i].equipo +
+                    "</td><td>" + data[i].amarillas + "</td></tr>";
+  }
+  document.getElementById("tablaAmarillas").innerHTML = tablaAmarillas;
+}
