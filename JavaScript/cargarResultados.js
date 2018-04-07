166e1c5e2cf1927b60fcd8b148aaefbb48f4181f
JUAN-PC\Juan <juuan.olmedo@hotmail.com>
2018-04-07 15:39:29 -0300

    Cargar resultados

---
+++ JavaScript/cargarResultados.js
@@ -0,0 +1,30 @@
+$(document).ready(getArchivo);
+
+function getArchivo(){
+  $.get("https://uns-iaw-2018-com16.github.io/TorneoFutbol/JSON/fixture.json", function(data, status){
+      crearResultados(data);
+  });
+}
+
+function crearResultados(data){
+  var i,j;
+  var panel="";
+  for(i = 0; i < data.length; i++){
+    panel += "<div class=\"panel panel-info\">"+
+            "<div class=\"panel-heading\">"+
+              "<p>Fecha "+(i+1)+"</p>"+
+            "</div>"+
+            "<div class=\"panel-body\">";
+    for(j = 0; j<data[i].length; j++){
+        var golesLocal,golesVisita;
+        if(data[i][j].golesLocal != null){
+          golesLocal = data[i][j].golesLocal;
+          golesVisita = data[i][j].golesVisita;
+          panel += "<p>"+data[i][j].equipoLocal+" - "+golesLocal+" - "+
+             " vs. - "+golesVisita+" - "+data[i][j].equipoVisitante+" </p>";
+        } 
+    }
+    panel+= "</div></div>";
+  }
+  document.getElementById("resultados").innerHTML = panel;
+}
