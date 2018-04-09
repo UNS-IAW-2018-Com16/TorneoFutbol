977b221e8647ae59b6926de41e764cca496d1953
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-04-09 11:20:46 -0300

    no message

--- JavaScript/cambiarEstilo.js
+++ JavaScript/cambiarEstilo.js
@@ -1,10 +1,10 @@
-$("#estilo1").click(function(){
+/* $("#estilo1").click(function(){
     estiloModoGeneral();   
 });
 
 $("#estilo2").click(function(){
     estiloModoOscuro();
-});
+});*/
 
 function estiloModoGeneral(){
     $('link[id="estiloPagina"]').attr('href','https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloGeneral.css');
