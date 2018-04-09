e2767cb361e5774432133f1e18de79d2edd910ad
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-04-09 17:08:43 -0300

    no message

--- JavaScript/cargarFixture.js
+++ JavaScript/cargarFixture.js
@@ -12,16 +12,29 @@ function crearFixture(data){
   for(i = 0; i < data.length; i++){
     panel += "<div class=\"panel panel-info\">"+
             "<div class=\"panel-heading\">"+
-              "<p>Fecha "+(i+1)+"</p>"+
+            "<p>Fecha "+(i+1)+"</p>"+
             "</div>"+
             "<div class=\"panel-body\">";
     for(j = 0; j<data[i].length; j++){
-        panel += "<p>"+data[i][j].equipoLocal+
-             " vs. "+data[i][j].equipoVisitante+
-             " - Fecha: "+data[i][j].fecha+
-             " - Hora: "+data[i][j].hora+
-             " - Cancha: "+data[i][j].cancha+
-             " - Arbitro: "+data[i][j].arbitro+" </p>";
+        panel += "<p>"+
+             "<div class='partidoFechaFixture'>"+
+             data[i][j].equipoLocal+
+             " - "+
+             data[i][j].equipoVisitante+
+             "</div>"+
+             "<div class='dateFechaFixture'>"+
+             "<img src='https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Iconos/date.png' alt='Fecha del partido' class='icono'>"+data[i][j].fecha+
+             "</div>"+
+             "<div class='timeFechaFixture'>"+
+             "<img src='https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Iconos/time.png' alt='Hora del partido' class='icono'>"+data[i][j].hora+
+             "</div>"+
+             "<div class='canchaFechaFixture'>"+
+             "<img src='https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Iconos/cancha.png' alt='Cancha del partido' class='icono'>"+data[i][j].cancha+
+             "</div>"+
+             "<div class='arbitroFechaFixture'>"+
+             "<img src='https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Iconos/arbitro.png' alt='Arbitro del partido' class='icono'>"+data[i][j].arbitro+
+             "</div>"+
+             "</p>";
     }
     panel+= "</div></div>";
   }
