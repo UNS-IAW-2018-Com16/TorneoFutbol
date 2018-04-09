5632f7459583309556a7ff86f7d84015aef8b6b7
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-04-09 10:55:32 -0300

    no message

--- JavaScript/cambiarEstilo.js
+++ JavaScript/cambiarEstilo.js
@@ -1,17 +1,16 @@
-$("#estilo2").click(function(){
-    estiloModoOscuro();
-});
-
-
 $("#estilo1").click(function(){
     estiloModoGeneral();   
 });
 
+$("#estilo2").click(function(){
+    estiloModoOscuro();
+});
+
 function estiloModoGeneral(){
-    $("link[id=\"estiloPagina\"]").attr("href","https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloGeneral.css");
+    $("#estiloPagina").attr("href","https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloGeneral.css");
 }
 
 
 function estiloModoOscuro(){
-    $("link[id=\"estiloPagina\"]").attr("href","https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloOscuro.css");    
+    $("#estiloPagina").attr("href","https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloOscuro.css");    
 }
