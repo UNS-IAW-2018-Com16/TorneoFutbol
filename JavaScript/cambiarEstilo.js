96953080035b1ee057cc9fe74fbc72a14f032edb
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-04-09 12:35:30 -0300

    no message

--- JavaScript/cambiarEstilo.js
+++ JavaScript/cambiarEstilo.js
@@ -3,38 +3,20 @@ $(document).ready(function() {
     var estiloCSS = localStorage.getItem("estiloActual");
     
     if (estiloCSS === null){
-        estiloModoGeneral();
+        cambiarEstilo('estiloGeneral.css');
     } else{
-        setHref(estiloCSS);
+        cambiarEstilo(estiloCSS);
     }
 
 });
     
+function cambiarEstilo(estilo){
+    setReferencia(estilo);
+    localStorage.setItem("estiloActual",estilo);
 
-/* $("#estilo1").click(function(){
-    estiloModoGeneral();   
-});
-
-$("#estilo2").click(function(){
-    estiloModoOscuro();
-});*/
-
-function estiloModoGeneral(){
-    localStorage.setItem("estiloActual","estiloGeneral.css");
-    //setHref("estiloGeneral.css");
-    
-    /*$('link[id="estiloPagina"]').attr('href','https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloGeneral.css');*/
-}
-
-
-function estiloModoOscuro(){
-    localStorage.setItem("estiloActual","estiloOscuro.css");
-    //setHref("estiloOscuro.css");
-    
-    /*$("link[id=\"estiloPagina\"]").attr("href","https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloOscuro.css"); */   
 }
     
-function setHref(estilo){
+function setReferencia(estilo){
     var rutaAbsoluta = "https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/"+estilo;
     $("link[id=\"estiloPagina\"]").attr("href",rutaAbsoulta);
         
