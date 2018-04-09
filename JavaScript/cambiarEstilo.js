a79df1aa3a0a5f88960dd84ee25855483e672d44
JUAN-PC\Juan <juuan.olmedo@hotmail.com>
2018-04-08 23:45:28 -0300

    no message

--- JavaScript/cambiarEstilo.js
+++ JavaScript/cambiarEstilo.js
@@ -1,17 +1,17 @@
-$("#estilo2").click(function() {
+$("#estilo2").click(function(){
     estiloModoOscuro();
 });
 
 
-$("#estilo1").click(function() {
+$("#estilo1").click(function(){
     estiloModoGeneral();   
 });
 
-function estiloModoGeneral () {
+function estiloModoGeneral(){
     $('link[id="estiloPagina"]').attr('href','https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloGeneral.css');
 }
 
 
-function estiloModoOscuro () {
+function estiloModoOscuro(){
     $('link[id="estiloPagina"]').attr('href','https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/estiloOscuro.css');    
 }
