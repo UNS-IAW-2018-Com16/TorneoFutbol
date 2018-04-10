22b74fb4c31c014eefa940f5da9848fd5e124a4b
DESKTOP-64CCSB0\Juanfri <juanfri.m@hotmail.com>
2018-04-10 16:39:11 -0300

    no message

--- JavaScript/cambiarEstilo.js
+++ JavaScript/cambiarEstilo.js
@@ -10,12 +10,18 @@ $(function() {
 function cambiarEstilo(estilo){
     setReferencia(estilo);
     localStorage.setItem("estiloActual",estilo);
+    cambiarTitulo(estilo);
 
 }
     
 function setReferencia(estilo){
     var rutaAbsoluta = "https://uns-iaw-2018-com16.github.io/TorneoFutbol/CSS/"+estilo;
     $("link[id=\"estiloPagina\"]").attr("href",rutaAbsoluta);
-        
-    
+}
+
+function cambiarTitulo(estilo){
+    if(estilo==="estiloGeneral.css")
+        $("img[id=\"titulo\"]").attr("src","https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Fondos/EstiloGeneral/banner.png");
+    else
+        $("img[id=\"titulo\"]").attr("src","https://uns-iaw-2018-com16.github.io/TorneoFutbol/Imagenes/Fondos/EstiloGeneral/banner.png");
 }
